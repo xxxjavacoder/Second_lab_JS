308f8b5a3096dc8b2e8a01692a3ad2fa11c7f5bf
xxxjavacoder <lytvyniuk.v@donnu.edu.ua>
2019-10-16 13:21:16 +0300

    1.1 complet!

---
+++ script.js
@@ -0,0 +1,14 @@
+'use strict'
+
+function hosting(){
+ var a = 8;
+for(var i = 0; i <= 100; i++){
+  a++;
+  console.log("Variable a is hosted!");
+  if(a >= 11){
+      break;
+  }
+ }
+}
+
+hosting();
