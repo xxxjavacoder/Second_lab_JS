adb3d0fda84d5a7e628f308267a92e4b5f900edf
xxxjavacoder <lytvyniuk.v@donnu.edu.ua>
2019-10-17 13:21:46 +0300

    Task 2.1 complete!

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 'use strict'
 
-function hosting(){
- var a = 8;
-for(var i = 0; i <= 100; i++){
-  a++;
-  console.log("Variable a is hosted!");
-  if(a >= 11){
-      break;
+ function hosting(){
+    var a = 8;
+ for(var i = 0; i <= 100; i++){
+   a++;
+   console.log("Variable a is hosted!");
+   if(a >= 11){
+       break;
+   }
   }
  }
-}
 
 hosting();
+//1
+
+
+function inc(n){
+  var N = n;
+  return N;
+}
+
+var a = 5;
+var b = inc(a);
+console.dir({a,b});
+
+//2.1
